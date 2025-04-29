a832c502ca1e8ad5bea716c3750312bafaabc6d3
neynn <nils.lucky@web.de>
2025-04-29 07:16:25 +0200

    Updated Codex

--- data/codex.js
+++ data/codex.js
@@ -1,91 +1,91 @@
 const CODEX = {
-	"Jokes": {
+	"JOKES": {
 		"DocName": "CODEX_JOKES_NAME",
 		"DocDesc": "CODEX_JOKES_DESC",
 		"DocText": "CODEX_JOKES_TEXT",
 		"DocSize": 2
 	},
-	"SpecialUnits": {
+	"SPECIAL_UNITS": {
 		"DocName": "CODEX_SPECIAL_UNITS_NAME",
 		"DocDesc": "CODEX_SPECIAL_UNITS_DESC",
 		"DocText": "CODEX_SPECIAL_UNITS_TEXT",
 		"DocSize": 3
 	},
-	"SpecialArmor": {
+	"SPECIAL_ARMOR": {
 		"DocName": "CODEX_SPECIAL_ARMOR_NAME",
 		"DocDesc": "CODEX_SPECIAL_ARMOR_DESC",
 		"DocText": "CODEX_SPECIAL_ARMOR_TEXT",
 		"DocSize": 2
 	},
-	"SpecialAircraft": {
+	"SPECIAL_AIRCRAFT": {
 		"DocName": "CODEX_SPECIAL_AIRCRAFT_NAME",
 		"DocDesc": "CODEX_SPECIAL_AIRCRAFT_DESC",
 		"DocText": "CODEX_SPECIAL_AIRCRAFT_TEXT",
 		"DocSize": 1
 	},
-	"SpecialShips": {
+	"SPECIAL_SHIPS": {
 		"DocName": "CODEX_SPECIAL_SHIPS_NAME",
 		"DocDesc": "CODEX_SPECIAL_SHIPS_DESC",
 		"DocText": "CODEX_SPECIAL_SHIPS_TEXT",
 		"DocSize": 2
 	},
-	"ThisWorld": {
+	"THIS_WORLD": {
 		"DocName": "CODEX_THIS_WORLD_NAME",
 		"DocDesc": "CODEX_THIS_WORLD_DESC",
 		"DocText": "CODEX_THIS_WORLD_TEXT",
 		"DocSize": 4
 	},
-	"ItHappened": {
+	"IT_HAPPENED": {
 		"DocName": "CODEX_IT_HAPPENED_NAME",
 		"DocDesc": "CODEX_IT_HAPPENED_DESC",
 		"DocText": "CODEX_IT_HAPPENED_TEXT",
 		"DocSize": 4
 	},
-	"Operation_TZIMTZUM": {
+	"OPERATION_TZIMTZUM": {
 		//Unknown
 		"DocName": "CODEX_OPERATION_TZIMTZUM_NAME",
 		"DocDesc": "CODEX_OPERATION_TZIMTZUM_DESC",
 		"DocText": "CODEX_OPERATION_TZIMTZUM_TEXT",
 		"DocSize": 3
 	},
-	"Operation_GOLCHAB": {
+	"OPERATION_GOLCHAB": {
 		//ACSF's plans to ensure Kargia never again has the practical means to pose a continental-scale threat.
 		"DocName": "CODEX_OPERATION_GOLCHAB_NAME",
 		"DocDesc": "CODEX_OPERATION_GOLCHAB_DESC",
 		"DocText": "CODEX_OPERATION_GOLCHAB_TEXT",
 		"DocSize": 3
 	},
-	"Operation_GHAAG_SHEBLAH": {
+	"OPERATION_GHAAG_SHEBLAH": {
 		//Unknown
 		"DocName": "CODEX_OPERATION_GHAAG_SHEBLAH_NAME",
 		"DocDesc": "CODEX_OPERATION_GHAAG_SHEBLAH_DESC",
 		"DocText": "CODEX_OPERATION_GHAAG_SHEBLAH_TEXT",
 		"DocSize": 3
 	},
-	"Operation_OHR_EIN_ZOF": {
+	"OPERATION_OHR_EIN_ZOF": {
 		//White Tulip's plan to force a quick, bloodless capitulation of Polasie
 		"DocName": "CODEX_OPERATION_OHR_EIN_ZOF_NAME",
 		"DocDesc": "CODEX_OPERATION_OHR_EIN_ZOF_DESC",
 		"DocText": "CODEX_OPERATION_OHR_EIN_ZOF_TEXT",
 		"DocSize": 3
 	},
-	"Operation_SAMSON": {
+	"OPERATION_SAMSON": {
 		//Calvary Institute's records regarding the top-secret PROIECT SAMSON
 		"DocName": "CODEX_OPERATION_SAMSON_NAME",
 		"DocDesc": "CODEX_OPERATION_SAMSON_DESC",
 		"DocText": "CODEX_OPERATION_SAMSON_TEXT",
 		"DocSize": 3
 	},
-	"Operation_THAUMIEL": {
+	"OPERATION_THAUMIEL": {
 		//White Tulip's plans to cause structural and moral damage to Polasian society
 		"DocName": "CODEX_OPERATION_THAUMIEL_NAME",
 		"DocDesc": "CODEX_OPERATION_THAUMIEL_DESC",
 		"DocText": "CODEX_OPERATION_THAUMIEL_TEXT",
 		"DocSize": 3
 	},
-	"War_Plan_ARMAGEDDON": {
+	"WAR_PLAN_ARMAGEDDON": {
 		//DATA EXPUNGED
-		"DocName": "CODEX_OPERATION_THAUMIEL_NAME",
+		"DocName": "CODEX_WAR_PLAN_ARMAGEDDON_NAME",
 		"DocDesc": "CODEX_WAR_PLAN_ARMAGEDDON_DESC",
 		"DocText": "CODEX_WAR_PLAN_ARMAGEDDON_TEXT",
 		"DocSize": 5
